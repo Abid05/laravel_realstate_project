cb26370df81b5d23636d84e83818bb31c2ac9e06
abid05 <sazzadhossainabid@gmail.com>
2023-12-27 20:40:01 +0600

    admin profile

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -28,5 +29,13 @@ class AdminController extends Controller
     public function AdminLogin(Request $request){
 
         return view('admin.admin_login');
+    }
+
+    //admin profile
+    public function AdminProfile(){
+
+        $id = Auth::user()->id;
+        $profileData = User::find($id);
+        return view('admin.admin_profile_view',compact('profileData'));
     }
 }
